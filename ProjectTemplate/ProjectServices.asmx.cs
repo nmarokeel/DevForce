b1862b48a30dec56a7acadf217d06a669ba1a468
Ted <ghosty221@yahoo.com>
2020-02-06 19:24:46 -0700

    uploaded Keilyn's web pages and supporting files as well as logon web method

--- ProjectTemplate/ProjectServices.asmx.cs
+++ ProjectTemplate/ProjectServices.asmx.cs
@@ -62,15 +62,36 @@ namespace ProjectTemplate
                 return "Something went wrong, please check your credentials and db name and try again.  Error: " + e.Message;
             }
         }
-        [WebMethod]
-        public Person getJason()
+        [WebMethod(EnableSession = true)]
+        public bool logOn(string uname, string pwd)
         {
-            Person jason = new Person();
-            jason.firstName = "Jason";
-            jason.lastName = "Nichols";
-            jason.phoneNumber = 1234567891;
+            bool success = false;
 
-            return jason;
+            //set the sql query to pass through and assign to the string sqlSelect
+            string sqlSelect = "SELECT userid FROM users WHERE email=@emailvalue AND password=@passvalue";
+
+            //set the sql connection to be used
+            MySqlConnection sqlConnection = new MySqlConnection(getConString());
+
+            //the command to be sent through
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+
+            //decode the values sent over from the sql server
+            sqlCommand.Parameters.AddWithValue("@emailvalue", HttpUtility.UrlDecode(uname));
+            sqlCommand.Parameters.AddWithValue("@passvalue", HttpUtility.UrlDecode(pwd));
+
+            //put the values received into a table and see if they match, if they do then we change our boolean to true, then return the value of the boolean
+            MySqlDataAdapter sqlDa = new MySqlDataAdapter(sqlCommand);
+            DataTable sqlDt = new DataTable();
+
+            sqlDa.Fill(sqlDt);
+            if (sqlDt.Rows.Count > 0)
+            {
+                Session["userid"] = sqlDt.Rows[0]["userid"];
+                success = true;
+            }
+
+            return success;
         }
     }
 }

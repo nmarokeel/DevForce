cd4253f4b53af544cdf61bb549048a9e8fb6042c
Harley <hgreytak@asu.edu>
2020-02-15 16:00:14 -0700

    base person class created

--- ProjectTemplate/Person.cs
+++ ProjectTemplate/Person.cs
@@ -7,8 +7,24 @@ namespace ProjectTemplate
 {
     public class Person
     {
-        public string firstName;
-        public string lastName;
-        public int phoneNumber;
+        private string fristName, lastName, email, password, status;
+        private int userID;
+
+        public Person(string fristName, string lastName, string email, string password, string status, int userID)
+        {
+            this.fristName = fristName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.status = status;
+            this.userID = userID;
+        }
+
+        public string FristName { get => fristName; set => fristName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public string Status { get => status; set => status = value; }
+        public int UserID { get => userID; set => userID = value; }
     }
 }
